fb8d93045382300c9f926d65f3030f6c6925938c
Abhay12911 <abhaystp30@gmail.com>
2025-09-30 15:53:12 +0530

    config update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
@@ -13,12 +13,14 @@ const queryClient = new QueryClient({
 	},
 });
 
+const Router = import.meta.env.DEV ? BrowserRouter : HashRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </Router>
+    </React.StrictMode>
 );
